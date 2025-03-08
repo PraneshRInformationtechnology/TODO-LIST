09413448339b1575f9a6e366490462ca72c3b1e8
pranesh <r.pranesh001@gmail.com>
2025-03-08 11:35:39 +0530

    finally

--- todobackend/server.js
+++ todobackend/server.js
@@ -8,7 +8,7 @@ const cors=require('cors');
 const app=express(); 
 app.use(express.json())
 const corsOptions ={
-    origin: process.env.APPLICATION_URL,
+    origin: process.env.APPLICATION_URL|| '*',
     methods : 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }
 
